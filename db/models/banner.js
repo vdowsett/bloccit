ceee0dd486670fce6ae75213d546b5bf2a2b908b
vdowsett <vdowsett@gmail.com>
2019-01-18 17:25:10 -0600

    partway commit

---
+++ db/models/banner.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Banner = sequelize.define('Banner', {
+    source: DataTypes.STRING,
+    description: DataTypes.STRING,
+    topicId: {
+      type: DataTypes.INTEGER,
+      onDelete: "CASCADE",
+      references: {
+        model: "Topics",
+        key: "id",
+        as: "topicId",
+      }
+    }
+  }, {});
+  Banner.associate = function(models) {
+    Banner.belongsTo(models.Topic, {
+      foreignKey: "topicId",
+      onDelete: "CASCADE",
+    });
+  };
+  return Banner;
+};
