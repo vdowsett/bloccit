8894f9b7a3850275c73dc27344f8d8288d0ed077
vdowsett <vdowsett@gmail.com>
2019-01-18 12:03:00 -0600

    adding /marco to spec

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,20 +4,26 @@ const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
 
-//#1
-  describe("GET /", () => {
+    describe("GET /", () => {
+        it("should return status code 200", (done) => {
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
 
-//#2
-    it("should return status code 200", (done) => {
-
-//#3
-      request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-
-//#4
-        done();
-      });
     });
 
-  });
+    describe("GET /marco", () => {
+        it("should return status code 200 and contain a string of 'polo'", (done) => {
+            request.get(base + "marco", (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toBe("polo");
+                done();
+            });
+        });
+
+    });
+
+
 });
