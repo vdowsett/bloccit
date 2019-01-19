1512f86d54c02fe1ee11e2ad82fa67a7e067f6b1
vdowsett <vdowsett@gmail.com>
2019-01-19 10:03:29 -0600

    server troubleshooting

--- src/server.js
+++ src/server.js
@@ -19,18 +19,6 @@ function normalizePort(val) {
     return false;
   }
 
-  if (process.env.DATABASE_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      logging:  true //false
-    });
-  } else {
-    // the application is executed on the local machine
-    sequelize = new Sequelize("postgres:///my_db");
-  }
-
 server.on("listening", () => {
     console.log(`server is listening for requests on port ${server.address().port}`);
 });
