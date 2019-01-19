face2215fd3001f37acf498778558d8fc000de0d
vdowsett <vdowsett@gmail.com>
2019-01-19 09:41:19 -0600

    adding further CRUD actions for the Topic resource

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,10 +1,12 @@
 const path = require("path");
- const viewsFolder = path.join(__dirname, "..", "views");
+const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
 
 module.exports = {
 init(app, express){
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(express.static(path.join(__dirname, "..", "assets")));
     }
 };
