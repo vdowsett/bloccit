0534811d172cc7d15dba085ced0c65f13a16a5d2
vdowsett <vdowsett@gmail.com>
2019-01-20 11:51:23 -0600

    created post model with tests

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,19 +1,37 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
+  
   var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+    
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
   }, {});
+
   Topic.associate = function(models) {
-    // associations can be defined here
+    
     Topic.hasMany(models.Banner, {
       foreignKey: "topicId",
       as: "banners",
     });
+
     Topic.hasMany(models.Rule, {
       foreignKey: "topicId",
       as: "rules",
     });
+
+    Topic.hasMany(models.Post, {
+      foreignKey: "topicID",
+      as: "posts"
+    });
+
   };
   return Topic;
 };
