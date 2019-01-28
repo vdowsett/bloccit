01ca961e43d1ca6cecb2a430af72502de7dc0021
vdowsett <vdowsett@gmail.com>
2019-01-28 11:47:55 -0600

    added new post view - getting errors

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,11 +3,13 @@ module.exports = {
 
       const staticRoutes = require("../routes/static");
       const topicRoutes = require("../routes/topics");
-      const advertisementRoutes = require("../routes/advertisements")
+      const advertisementRoutes = require("../routes/advertisements");
+      const postRoutes = require("../routes/posts");
      
       app.use(staticRoutes);
       app.use(topicRoutes);
       app.use(advertisementRoutes);
+      app.use(postRoutes);
       
     }
   }
---
+++ src/routes/posts.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const postController = require("../controllers/postController");
+
+router.get("/topics/:topicId/posts/new", postController.new);
+
+module.exports = router;
